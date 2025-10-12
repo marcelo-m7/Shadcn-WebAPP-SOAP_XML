0e13dab82a6cf3ad1b1141c626945457c1759c9c
Marcelo Santos <marcelo@monynha.com>
2025-10-12 10:36:02 +0100

    [dyad] Recreated the Label component file and requested a rebuild to resolve the import error. - wrote 1 file(s)

--- apps/web/src/components/ui/label.tsx
+++ apps/web/src/components/ui/label.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
 import { cva, type VariantProps } from "class-variance-authority"
@@ -8,12 +10,10 @@ const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
 )
 
-interface LabelProps extends React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>,
-    VariantProps<typeof labelVariants> {}
-
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
-  LabelProps
+  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
+    VariantProps<typeof labelVariants>
 >(({ className, ...props }, ref) => (
   <LabelPrimitive.Root
     ref={ref}
