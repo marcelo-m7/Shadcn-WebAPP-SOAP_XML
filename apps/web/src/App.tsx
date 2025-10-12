b1bb02a2142c804f935ef49c43a343aecedcdbc1
Marcelo Santos <marcelo@monynha.com>
2025-10-12 10:18:41 +0100

    [dyad] Created the web application structure and updated workspace configuration - wrote 11 file(s)

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import IndexPage from './pages/Index.tsx';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-50">
+      <Routes>
+        <Route path="/" element={<IndexPage />} />
+        {/* Add other routes here */}
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
